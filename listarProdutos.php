fcfe10b315e3e157dd9acad3c60f9eef1176f462
JoaoDias06 <joaozinhopd06@gmail.com>
2025-04-08 19:51:27 -0300

    adicao de botao de inserçao de produto
    adicao de botao de inserçao de produto

--- listarProdutos.php
+++ listarProdutos.php
@@ -1,8 +1,8 @@
 <?php 
     include 'conexao.php';
     $pdo = conexao::conectar();
-    $sql = "SELECT * FROM produto";
-    $listarprodutos = $pdo->query($sql);
+    $sql1 = "SELECT * FROM produto JOIN categoria ON produto.CATEGORIA_PRODUTO = categoria.ID_CATEGORIA JOIN marca ON produto.MARCA_PRODUTO = marca.ID_MARCA";
+    $listarprodutos = $pdo->query($sql1);
     conexao::desconectar();
 ?>
 
@@ -18,37 +18,43 @@
 </head>
 <body>
     <div class="col s12">
-        <h4 class="card-panel teal lighten-2 white-text align center">Produtos no Estoque</h4>        
+        <h4 class="card-panel teal lighten-2 white-text align center">Produtos no Estoque  
+            <a class="btn-floating btn-medium waves-effect waves-light green hoverable" onclick="Javascript:location.href='frminsProduto.php'">
+            <i class="material-icons">add</i></a>
+        </h4>
         
         <div class="container">
             <table>
-                    <tr>
-                        <th>ID</th>
-                        <th class="center align">NOME</th>
-                        <th class="center align">CATEGORIA</th>
-                        <th class="center align">MARCA</th>
-                        <th class="center align">PREÇO</th>
-                        <th class="center align">QUANTIDADE</th>
-                    </tr>
+                <tr>
+                    <th>ID</th>
+                    <th class="center align">NOME</th>
+                    <th class="center align">CATEGORIA</th>
+                    <th class="center align">MARCA</th>
+                    <th class="center align">PREÇO</th>
+                    <th class="center align">QUANTIDADE</th>
+                </tr>
 
-                    <?php 
-                        $cont = 0;
-                        foreach ($listarprodutos as $produto) {
-                    ?>
-                    <tr class="grey lighten-3">
-                        <td><?php echo $produto['ID']?></td>
-                        <td class="center align"><?php echo $produto['NOME']?></td>
-                        <td class="center align"><?php echo $produto['CATEGORIA']?></td>
-                        <td class="center align"><?php echo $produto['MARCA']?></td>
-                        <td class="center align"><?php echo $produto['PRECO']?></td>
-                        <td class="center align"><?php echo $produto['QUANTIDADE']?></td>
-                    </tr>
-                    <?php 
-                        $cont++;
-                        }
-                    ?>
+                <?php 
+                    $cont = 0;
+                    foreach ($listarprodutos as $produto) {
+                ?>
+                <tr class="grey lighten-3">
+                    <td><?php echo $produto['ID_PRODUTO']?></td>
+                    <td class="center align"><?php echo $produto['NOME_PRODUTO']?></td>
+                    <td class="center align"><?php echo $produto['NOME_CATEGORIA']?></td>
+                    <td class="center align"><?php echo $produto['NOME_MARCA']?></td>
+                    <td class="center align"><?php echo $produto['PRECO_PRODUTO']?></td>
+                    <td class="center align"><?php echo $produto['QUANTIDADE_PRODUTO']?></td>
+                </tr>
+                <?php 
+                    $cont++;
+                    }
+                ?>
             </table>
         </div>
+        
+        
+        <h6 class="center align">O Número de Itens no Estoque é de <?php echo $cont?></h6>
     </div>
 </body>
 </html>
