4831b57dd05b1913175cf11c0f1450dca5003e31
JoaoDias06 <joaozinhopd06@gmail.com>
2025-04-24 19:40:56 -0300

    correçao na remoçao de produtos
    correçao na remoçao de produtos

--- frmremProduto.php
+++ frmremProduto.php
@@ -1,11 +1,12 @@
 <?php 
+    $id = $_GET['id'];
     include 'conexao.php';
     $pdo = conexao::conectar();
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM produto WHERE ID_PRODUTO=?";
+    $sql = "SELECT * FROM produto JOIN categoria ON produto.CATEGORIA_PRODUTO = categoria.ID_CATEGORIA JOIN marca ON produto.MARCA_PRODUTO = marca.ID_MARCA WHERE ID_PRODUTO=?";
     $query = $pdo->prepare($sql);
     $query->execute(array($id));
     $produto = $query->fetch(PDO::FETCH_ASSOC);
+    conexao::desconectar();
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +25,28 @@
 
         <div class="container">
             <h5 class="align center">Informações do Produto</h5>
-            <h6>ID: <?php $produto['ID_PRODUTO'] ?></h6> 
+            <br>
+            <h6>ID: <?php echo $id?></h6>
+            <h6>NOME: <?php echo $produto['NOME_PRODUTO']?></h6>
+            <h6>CATEGORIA: <?php echo $produto['NOME_CATEGORIA']?></h6>
+            <h6>MARCA: <?php echo $produto['NOME_MARCA']?></h6>
+            <h6>PREÇO <?php echo $produto['PRECO_PRODUTO']?></h6>
+            <h6>QUANTIDADE: <?php echo $produto['QUANTIDADE_PRODUTO']?></h6>
+            
+            <div class="row">
+                <form action="remProduto.php" method="POST" id="frmremProduto">
+                    <div class="input-field">
+                        <br>
+                        <button class="btn waves-effect waves-light red" type="submit" name="btnGravar">Deletar
+                            <i class="material-icons right">delete</i>
+                        </button>
+                        <button class="btn waves-effect waves-light green" type="button" name="btnVoltar"
+                                onclick="JavaScript:location.href='listarProdutos.php'">Voltar
+                                <i class="material-icons right">arrow_back</i>    
+                        </button>
+                    </div>    
+                </form>
+            </div>
         </div>
     </div>
 </body>
