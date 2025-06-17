a822bdc27534b0658550a502b7824574dc52e475
JoaoDias06 <joaozinhopd06@gmail.com>
2025-06-17 08:38:04 -0300

    Menu - Em Andamento
    Menu - Em Andamento

---
+++ menu.php
@@ -0,0 +1,55 @@
+<?php //verificar se o usuario tem acesso 
+  session_start();
+  if (!isset($_SESSION['usuario']))
+    header("location: index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Estoque - Menu Principal</title>
+</head>
+<body>
+  <!--Menu de Navegacao-->
+  <nav class="teal darken-4">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="../Produto/listarProdutos.php">Produtos</a></li>
+        <li><a href="../Categoria/listarCategorias.php">Categorias</a></li>
+        <li><a href="../Marca/listarMarcas.php">Marcas</a></li>
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <!--sidenav-->
+  <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="../imagens/industria.png" width="300px" height="180px">
+      </div>
+      <a href="#name"><span class="white-text name">João Dias</span></a>
+      <a href="#email"><span class="white-text email">joaopedrodias0601@gmail.com</span></a>
+    </div></li>
+    <li><a href="listarCompetidores.php"><i class="material-icons">person</i>COMPETIDOR</a></li>
+    <li><a href="#!"><i class="material-icons">event</i>RODEIOS</a></li>
+    <li><div class="divider"></div></li>
+    <li><a href="logout.php"><i class="material-icons">exit_to_app</i>Logout</a></li>
+   </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons teal-text">menu</i></a>
+   
+</body>
+</html>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+</script>
