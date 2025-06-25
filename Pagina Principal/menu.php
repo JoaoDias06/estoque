9cf952d534dff9e6eaf412d39af4a67544f327e5
JoaoDias06 <joaozinhopd06@gmail.com>
2025-06-25 20:34:15 -0300

    Adição de Movimentação e home no menu
    Adição de Movimentação e home no menu

--- Pagina Principal/menu.php
+++ Pagina Principal/menu.php
@@ -1,7 +1,7 @@
 <?php //verificar se o usuario tem acesso 
   session_start();
   if (!isset($_SESSION['usuario']))
-    header("location:../index.php");
+    header("location:../Login/index.php");
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +24,8 @@
         <li><a href="../Produto/listarProdutos.php">Produtos</a></li>
         <li><a href="../Categoria/listarCategorias.php">Categorias</a></li>
         <li><a href="../Marca/listarMarcas.php">Marcas</a></li>
-        <li><a href="../logout.php">Logout</a></li>
+        <li><a href="../Movimentacao/listarMovimento.php">Movimentação</a></li>
+        <li><a href="../Login/logout.php">Logout</a></li>
       </ul>
     </div>
   </nav>
@@ -38,11 +39,13 @@
       <a href="#name"><span class="white-text name">João Dias</span></a>
       <a href="#email"><span class="white-text email">joaopedrodias0601@gmail.com</span></a>
     </div></li>
+    <li><a href="../Pagina Principal/home.php"><i class="material-icons">home</i>Home</a></li>
     <li><a href="../Produto/listarProdutos.php"><i class="material-icons">business_center</i>Produtos</a></li>
     <li><a href="../Categoria/listarCategorias.php"><i class="material-icons">collections_bookmark</i>Categorias</a></li>
     <li><a href="../Marca/listarMarcas.php"><i class="material-icons">event_note</i>Marcas</a></li>
+    <li><a href="../Movimentacao/listarMovimento.php"><i class="material-icons">storage</i>Movimentação</a></li>
     <li><div class="divider"></div></li>
-    <li><a href="../logout.php"><i class="material-icons">exit_to_app</i>Logout</a></li>
+    <li><a href="../Login/logout.php"><i class="material-icons">exit_to_app</i>Logout</a></li>
    </ul>
   <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons teal-text">menu</i></a>
    
